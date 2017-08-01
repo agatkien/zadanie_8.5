79581a6a4ca904f251b7a30c85bb553b76655a94
Agata Snoch <agatasnoch@Agatas-MacBook-Air.local>
2017-08-01 16:49:18 +0400

    stworzenie pliku do odliczania trojkata

---
+++ script.js
@@ -0,0 +1,7 @@
+var a, h, triangleArea;
+a = prompt('podaj podstawe trojkata');
+h = prompt('podaj wysokosc trojkata')
+triangleArea = a*h/2
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
